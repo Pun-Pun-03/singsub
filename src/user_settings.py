dfbcb1c55d29554f7012789a8d739d884a5d1f32
Pun-Pun-03 <109609472+Pun-Pun-03@users.noreply.github.com>
2025-11-08 19:46:25 +0330

    Update user_settings.py

--- src/user_settings.py
+++ src/user_settings.py
@@ -3,20 +3,20 @@
 # List of source URLs to fetch proxy configurations from.
 # Add or remove URLs as needed. All URLs in this list are automatically enabled.
 SOURCE_URLS = [
-   #"https://drive.google.com/uc?id=1t6JEHK87a0U-jTzw3n_M7u_fKA2N1LTY&export=download#%E2%AD%90%EF%B8%8FFreeXrayVPN%E2%AD%90%EF%B8%8F"
+   "https://drive.google.com/uc?id=1t6JEHK87a0U-jTzw3n_M7u_fKA2N1LTY&export=download#%E2%AD%90%EF%B8%8FFreeXrayVPN%E2%AD%90%EF%B8%8F"
    #"https://raw.githubusercontent.com/miladtahanian/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs.txt",
-   "https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs_tested.txt",
+   #"https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs_tested.txt",
    #"https://raw.githubusercontent.com/4n0nymou3/multi-proxy-config-fetcher/refs/heads/main/configs/proxy_configs.txt"
-   "https://t.me/s/v2rayngvpn",
-   "https://t.me/s/unlimited_v2ray_ng",
-   "https://t.me/s/NoForcedHeaven",
-"https://raw.githubusercontent.com/Farid-Karimi/Config-Collector/refs/heads/main/mixed_iran.txt",
-"https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Splitted-By-Protocol/ss.txt",
+   #"https://t.me/s/v2rayngvpn",
+   #"https://t.me/s/unlimited_v2ray_ng",
+   #"https://t.me/s/NoForcedHeaven",
+#"https://raw.githubusercontent.com/Farid-Karimi/Config-Collector/refs/heads/main/mixed_iran.txt",
+#"https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/Splitted-By-Protocol/ss.txt",
    #"https://t.me/s/manVPN",
    #"https://t.me/s/entryNET",
    #"https://t.me/s/V2raysCollector",
-   "https://t.me/s/NETMelliAnti",
-   "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_1.txt"
+   #"https://t.me/s/NETMelliAnti",
+   #"https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mci/sub_1.txt"
    # Add more URLs here if you want to include additional sources.
 ]
 
